a7639ec9e045b571516d6c52cd0eec18a9c59d66
magechaP <magechapeter@gmail.com>
2019-06-05 22:58:04 +0300

    add a main route and a login decorator

--- app/auth/views.py
+++ app/auth/views.py
@@ -18,3 +18,28 @@ def index():
    posts = Post.query.all()
 
    return render_template('index.html', title= title, posts = posts)
+
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    return render_template("profile/profile.html", user = user)
+
+@main.route('/user/<uname>/update',methods = ['GET','POST'])
+@login_required
+def update_profile(uname):
+    user = User.query.filter_by(username = uname).first()
+    if user is None:
+        abort(404)
+
+    form = UpdateProfile()
+
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('.profile',uname=user.username))
+
+    return render_template('profile/update.html',form =form)    
