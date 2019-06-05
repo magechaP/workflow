1dfc0d47ec04f4a90eba5a3ca2927473b9a1c1b5
magechaP <magechapeter@gmail.com>
2019-06-05 22:54:20 +0300

    start on the decorators

--- app/auth/views.py
+++ app/auth/views.py
@@ -7,3 +7,14 @@ from ..models import User, Post, Comment
 from .forms import UpdateProfile, PostForm , CommentForm
 from .. import db, photos
 app = Flask(__name__)
+
+# views
+@main.route("/")
+def index():
+   '''
+   title = "Work Flow"
+   '''
+   title = 'Work Flow'
+   posts = Post.query.all()
+
+   return render_template('index.html', title= title, posts = posts)
