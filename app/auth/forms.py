4edb6f956e557a5b24bbd8c2d153bceb5460d6c0
magechaP <magechapeter@gmail.com>
2019-06-05 22:41:53 +0300

    create a post form class

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,3 +1,9 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField, SubmitField, SelectField
 
+class PostForm(FlaskForm):
+
+    title = StringField('Channel title')
+    category = SelectField('Channel category', choices=[('select a channel', 'select a channel'),('Management','Management'),('H.R','H.R'),('Finance','Finance'),('Maintenance','Maintainance'),('The Hall','The Hall')])
+    content = TextAreaField('Work Flow...')
+    submit = SubmitField('post')
