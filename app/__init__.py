6c872ad6a6b8664ddca96ccdb0f66d188b5c2004
Doktatech <Doktatech2@gmail.com>
2019-06-06 20:09:15 +0300

    Initiliaze app and register app blueprints

--- app/__init__.py
+++ app/__init__.py
@@ -1,2 +1,28 @@
 from flask import Flask
-app = Flask(__name__)
+from config import config_options
+from flask_bootstrap import Bootstrap
+# from flask_sqlalchemy import SQLAlchemy
+# from flask_login import LoginManager
+# login_manager = LoginManager()
+# login_manager.session_protection = 'strong'
+# login_manager.login_view = 'auth.login'
+
+# db = SQLAlchemy()
+bootstrap = Bootstrap()
+def create_app(config_name):
+    # initializing flask application
+    app = Flask(__name__)
+    # Setting up configurations
+    app.config.from_object(config_options[config_name])
+    # initializing flask extensions
+    bootstrap.init_app(app)
+    # db.init_app(app)
+    # login_manager.init_app(app)
+    # Registering Blueprints
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+    # from .auth import auth as auth_blueprint
+    # app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
+
+
+    return app
